40db99c400ac60bd936b9c485909f6c726dada3a
CSharperMantle <2649266980@qq.com>
2018-08-25 22:06:45 +0800

    Added UploadData.php and index

--- website/html/scripts/UploadData.php
+++ website/html/scripts/UploadData.php
@@ -6,6 +6,8 @@ GET Params:
   air_light 空气亮度
   ground_hum 地面湿度
 */
-
-
+  $air_temp = $_GET["air_temp"];
+  $air_hum = $_GET["air_hum"];
+  $light = $_GET["air_light"];
+  $ground_hum = $_GET["ground_hum"];
 ?>
