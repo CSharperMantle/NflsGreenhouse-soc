e1fcb525d96e1c6aca5134e07867623dcb7fef7a
CSharperMantle <2649266980@qq.com>
2018-08-28 14:33:25 +0800

    Finished MySQL connecting algs.

--- website/html/scripts/shared.php
+++ website/html/scripts/shared.php
@@ -1,11 +1,17 @@
 <?php
-    $host = 'db.example.com';
+    $host = 'localhost:3306';
     $dbname = 'plant_data';
     $passwd = 'plant_client';
     $username = 'plant_client';
+    $dbdsn = "mysql:host=$host;dbname=$dbname";
 
     $INSERT_SQL = 'INSERT INTO data (air_temp, air_hum, air_light, ground_hum) VALUES (?, ?, ?, ?);';
-    $FETCH_SQL = 'SELECT * FROM data WHERE id=MAX(id);';
+    $FETCH_SQL = 'SELECT * FROM data WHERE data.id = (SELECT MAX(id) FROM data);';
+    $FETCH_TOTAL_COUNT_SQL = 'SELECT MAX(id) FROM data;';
+
+    $lightSwitchValve = 500;
+    $groundHumSwitchValve = 500;
+    //TODO: Real value needed.
 
     function purifyData($data) {
         $data = trim($data);
--- website/html/upload.php
+++ website/html/upload.php
@@ -6,20 +6,17 @@
     air_light 空气亮度
     ground_hum 地面湿度
   */
-  require 'dbcommon.php';
+  require 'scripts\\shared.php';
 
   $air_temp = purifyData($_GET['air_temp']);
   $air_hum = purifyData($_GET['air_hum']);
-  $light = purifyData($_GET['air_light']);
+  $air_light = purifyData($_GET['air_light']);
   $ground_hum = purifyData($_GET['ground_hum']);
 
   try {
-    $db = new PDO('mysql:host=$host;dbname=$dbname', $username, $passwd);
+    $db = new PDO($dbdsn, $username, $passwd);
     $statement = $db->prepare($INSERT_SQL);
-    $statement->bind_param("ssss", $air_temp, $air_hum, $air_light, $ground_hum);
-    $statement->execute();
-    $statement->close();
-    $db->close();
+    $statement->execute(array($air_temp, $air_hum, $air_light, $ground_hum));
   }
   catch (PDOException $e) {
     exit(500);
