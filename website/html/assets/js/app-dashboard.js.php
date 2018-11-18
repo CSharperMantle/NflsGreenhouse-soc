e911644fef96b20ca4a37d254f463353b098fc5d
CSharperMantle <2649266980@qq.com>
2018-11-18 18:48:05 +0800

    Fixed bugs

--- website/html/assets/js/app-dashboard.js.php
+++ website/html/assets/js/app-dashboard.js.php
@@ -159,8 +159,40 @@ var App = (function () {
     //Row 1
     sparklines();
 
-    //Row 5
+    //Row 2
     map();
+
+  };
+
+  App.dataTables = function( ){
+
+    //We use this to apply style to certain elements
+    $.extend( true, $.fn.dataTable.defaults, {
+      dom:
+        "<'row be-datatable-header'<'col-sm-6'l><'col-sm-6'f>>" +
+        "<'row be-datatable-body'<'col-sm-12'tr>>" +
+        "<'row be-datatable-footer'<'col-sm-5'i><'col-sm-7'p>>"
+    } );
+
+    $("#table1").dataTable();
+
+    //Remove search & paging dropdown
+    $("#table2").dataTable({
+      pageLength: 5,
+      dom:  "<'row be-datatable-body'<'col-sm-12'tr>>" +
+            "<'row be-datatable-footer'<'col-sm-5'i><'col-sm-7'p>>"
+    });
+
+    //Enable toolbar button functions
+    $("#table3").dataTable({
+      buttons: [
+        'copy', 'excel', 'pdf', 'print'
+      ],
+      "lengthMenu": [[6, 10, 25, 50, -1], [6, 10, 25, 50, "All"]],
+      dom:  "<'row be-datatable-header'<'col-sm-6'l><'col-sm-6 text-right'B>>" +
+            "<'row be-datatable-body'<'col-sm-12'tr>>" +
+            "<'row be-datatable-footer'<'col-sm-5'i><'col-sm-7'p>>"
+    });
 
   };
 
