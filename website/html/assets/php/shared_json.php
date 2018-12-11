563d17a8c2b3ba61160241881385c4abd56d4054
CSharperMantle <2649266980@qq.com>
2018-12-11 12:41:46 +0800

    Fixed bugs

--- website/html/assets/php/shared_json.php
+++ website/html/assets/php/shared_json.php
@@ -8,4 +8,38 @@
     function create_action_array(int $action_type, int $target_id, string $param) {
         return array('action_type' => $action_type, 'target_id' => $target_id, 'param' => $param);
     }
+
+    class AlertType {
+        public const AIR_TEMP = 0;
+        public const AIR_HUM = 1;
+        public const AIR_LIGHT = 2;
+        public const GROUND_HUM = 3;
+        public const OK = 0;
+        public const HIGH = 1;
+        public const LOW = 2;
+    }
+
+    class AlertInfo {
+        public const GOOD = 0;
+        public const INFO = 1;
+        public const WARNING = 2;
+        public const DANGER = 3;
+    }
+    
+    class ActionType {
+        public const RELAY_ACTION = 1;
+        public const DEVICE_ACTION = 2;
+        public const RETRANSMIT_ACTION = 3;
+    }
+
+    class RelayAction {
+        public const ON = 1;
+        public const OFF = 0;
+    }
+
+    class SkySheetAction {
+        public const MOVEON = 1;
+        public const MOVEOFF = 0;
+        public const STOP = 2;
+    }
 ?>
