320acb5aa30b1b29fce62bf06ff50ae7700dcf13
CSharperMantle <2649266980@qq.com>
2018-12-16 18:40:49 +0800

    Fixed bugs

--- website/html/assets/js/app-dashboard-ajax.js
+++ website/html/assets/js/app-dashboard-ajax.js
@@ -4,7 +4,7 @@ var App = (() => {
     App.ajax_apply = function () {
         setInterval(() => {
             $.get('api/internal/ajax-history-data-table.php', (result) => {
-                $('history-data-table-tbody').html = result;
+                $('#history-data-table-tbody').html = result;
             });
         }, 1000);
     }
