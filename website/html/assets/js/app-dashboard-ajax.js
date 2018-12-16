218fad940931641bd4aa2cdf05d90bb39bcf94ba
CSharperMantle <2649266980@qq.com>
2018-12-16 19:26:49 +0800

    Reduced file size

--- website/html/assets/js/app-dashboard-ajax.js
+++ website/html/assets/js/app-dashboard-ajax.js
@@ -3,13 +3,7 @@ var App = (() => {
 
     App.ajax_apply = function () {
         setInterval(() => {
-            /*$.get('api/internal/ajax-history-data-table.php', (result) => {
-                $('#history-data-table-tbody').html = result;
-                console.log(result);
-            });*/
-            //$('#history-data-table-tbody').load('api/internal/ajax-history-data-table.php');
             $('#alert-div').load('api/internal/ajax-alert-div.php');
-            console.log('ajaxed');
         }, 1000);
     }
 
