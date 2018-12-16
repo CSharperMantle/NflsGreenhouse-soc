332376433caaaa92d414472d496490ade65eedf1
CSharperMantle <2649266980@qq.com>
2018-12-16 19:26:16 +0800

    Reduced load time

--- website/html/index.php
+++ website/html/index.php
@@ -28,14 +28,6 @@
     $today_uploads = $result['count'];
     $result = run_query_fetch($db, FETCH_TODAY_ALERTS_COUNT_SQL);
     $today_alerts = $result['count'];
-    $result = run_query_fetch($db, FETCH_LATEST_AIR_TEMP_ALERT_SQL);
-    $air_temp_is_ok = $result['is_ok'];
-    $result = run_query_fetch($db, FETCH_LATEST_AIR_HUM_ALERT_SQL);
-    $air_hum_is_ok = $result['is_ok'];
-    $result = run_query_fetch($db, FETCH_LATEST_AIR_LIGHT_ALERT_SQL);
-    $air_light_is_ok = $result['is_ok'];
-    $result = run_query_fetch($db, FETCH_LATEST_GROUND_HUM_ALERT_SQL);
-    $ground_hum_is_ok = $result['is_ok'];
 
     $all_commits = run_query_fetch_multi($db, FETCH_ALL_HISTORY_DATA_SQL);
     
@@ -201,57 +193,6 @@
               print_alert(AlertInfo::DANGER, '错误！', '加载页面时出错。页面不会正常工作。');
               http_response_code(503);
               exit();
-            }
-            switch ($air_temp_is_ok) {
-              case AlertType::HIGH:
-                print_alert(AlertInfo::WARNING, '警告!', '空气温度过高。');
-                $is_all_fine = false;
-                break;
-              case AlertType::LOW:
-                print_alert(AlertInfo::WARNING, '警告!', '空气温度过低。');
-                $is_all_fine = false;
-                break;
-              default:
-                break;
-            }
-            switch ($air_hum_is_ok) {
-              case AlertType::HIGH:
-                print_alert(AlertInfo::WARNING, '警告!', '空气湿度过高。');
-                $is_all_fine = false;
-                break;
-              case AlertType::LOW:
-                print_alert(AlertInfo::WARNING, '警告!', '空气湿度过低。');
-                $is_all_fine = false;
-                break;
-              default:
-                break;
-            }
-            switch ($air_light_is_ok) {
-              case AlertType::HIGH:
-                print_alert(AlertInfo::WARNING, '警告!', '光线过强。');
-                $is_all_fine = false;
-                break;
-              case AlertType::LOW:
-                print_alert(AlertInfo::WARNING, '警告!', '光线过弱。');
-                $is_all_fine = false;
-                break;
-              default:
-                break;
-            }
-            switch ($ground_hum_is_ok) {
-              case AlertType::HIGH:
-                print_alert(AlertInfo::WARNING, '警告!', ' 土壤过湿。');
-                $is_all_fine = false;
-                break;
-              case AlertType::LOW:
-                print_alert(AlertInfo::WARNING, '警告!', '土壤过干。');
-                $is_all_fine = false;
-                break;
-              default:
-                break;
-            }
-            if (!isset($is_all_fine)) {
-              print_alert(AlertInfo::GOOD, '放心！', '一切正常。');
             }
         ?>
         </div>
