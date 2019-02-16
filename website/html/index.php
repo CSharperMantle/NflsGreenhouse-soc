b4ec63a411f3b317fa4096931db1ae98d90958d8
CSharperMantle <2649266980@qq.com>
2019-02-16 11:02:44 +0800

    minor fixes

--- website/html/index.php
+++ website/html/index.php
@@ -577,7 +577,6 @@
   <script src="assets/lib/jquery/jquery.min.js" type="text/javascript"></script>
   <script src="assets/lib/perfect-scrollbar/js/perfect-scrollbar.jquery.min.js" type="text/javascript"></script>
   <script src="assets/lib/bootstrap/dist/js/bootstrap.bundle.min.js" type="text/javascript"></script>
-  <script src="assets/js/app.min.js" type="text/javascript"></script>
   <script src="assets/lib/jquery-flot/jquery.flot.js" type="text/javascript"></script>
   <script src="assets/lib/jquery-flot/jquery.flot.pie.js" type="text/javascript"></script>
   <script src="assets/lib/jquery-flot/jquery.flot.time.js" type="text/javascript"></script>
@@ -586,12 +585,11 @@
   <script src="assets/lib/jquery-flot/plugins/curvedLines.js" type="text/javascript"></script>
   <script src="assets/lib/jquery-flot/plugins/jquery.flot.tooltip.js" type="text/javascript"></script>
   <script src="assets/lib/jquery.sparkline/jquery.sparkline.min.js" type="text/javascript"></script>
-  <script src="assets/lib/countup/countUp.min.js" type="text/javascript"></script>
   <script src="assets/lib/jquery-ui/jquery-ui.min.js" type="text/javascript"></script>
   <script src="assets/lib/jqvmap/jquery.vmap.min.js" type="text/javascript"></script>
   <script src="assets/lib/jqvmap/maps/jquery.vmap.world.js" type="text/javascript"></script>
+  <script src="assets/lib/countup/countUp.min.js" type="text/javascript"></script>
   <script src="assets/lib/chartjs/Chart.min.js" type="text/javascript"></script>
-  <script src="assets/js/app-index.js" type="text/javascript"></script>
   <script src="assets/lib/datatables/datatables.net/js/jquery.dataTables.js" type="text/javascript"></script>
   <script src="assets/lib/datatables/datatables.net-bs4/js/dataTables.bootstrap4.js" type="text/javascript"></script>
   <script src="assets/lib/datatables/datatables.net-buttons/js/dataTables.buttons.min.js" type="text/javascript"></script>
@@ -600,6 +598,8 @@
   <script src="assets/lib/datatables/datatables.net-buttons/js/buttons.print.min.js" type="text/javascript"></script>
   <script src="assets/lib/datatables/datatables.net-buttons/js/buttons.colVis.min.js" type="text/javascript"></script>
   <script src="assets/lib/datatables/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js" type="text/javascript"></script>
+  <script src="assets/js/app.min.js" type="text/javascript"></script>
+  <script src="assets/js/app-index.js" type="text/javascript"></script>
 </body>
 
 </html>
