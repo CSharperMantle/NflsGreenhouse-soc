7207c54c354859eb0bbb2bad8c4b6cc7852ba44d
CSharperMantle <2649266980@qq.com>
2019-05-15 12:38:58 +0800

    add v1.1 of soc

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,7 @@
 #include <LiquidCrystal_I2C.h>
 #include <pt.h>
 #include <cJSON.h>
+#include <time.h>
 #include <packet_defs.hpp>
 #include <logger.hpp>
 #include <http_parser.h>
@@ -63,6 +64,10 @@ const PROGMEM int webServerPort = 80;
 const PROGMEM IPAddress webServerIp = IPAddress(10, 24, 141, 75);
 
 const byte mac[] = {0xB0, 0x83, 0xFE, 0x69, 0x1C, 0x9A};
+
+bool pinState[14];
+
+struct tm *p;
 
 LiquidCrystal_I2C *screen = new LiquidCrystal_I2C(0x27, 16, 2);
 DHT *airSensor = new DHT();
@@ -110,9 +115,11 @@ void parseJsonCJson(const char *str) {
                     if (param == 0) {
                         CONST_CAST(logger, Logger *)->Debug(String("OFF action on ") + String(target_id));
                         digitalWrite(target_id, LOW);
+                        pinState[target_id] = false;
                     } else {
                         CONST_CAST(logger, Logger *)->Debug(String("ON action on ") + String(target_id));
                         digitalWrite(target_id, HIGH);
+                        pinState[target_id] = true;
                     }
             }
             break;
@@ -184,7 +191,7 @@ HTTP_PARSER_CALLBACK(onMessageEndCallback(http_parser *parser)) {
 }
 
 void printLicenseInfo() {
-    CONST_CAST(logger, Logger *)->Info("This project is made by Mantle & iRed_K. Licensed under GPLv3.");
+    CONST_CAST(logger, Logger *)->Info("This project is made by Mantle & OverJerry & iRed_K. Licensed under GPLv3.");
     CONST_CAST(logger, Logger *)->Info("Libs in use:");
     CONST_CAST(logger, Logger *)->Info("cJSON by Dave Gamble and other cJSON contributors");
     CONST_CAST(logger, Logger *)->Info("ProtoThreads by Adam Dunkels");
@@ -198,6 +205,8 @@ void printLicenseInfo() {
     delay(1000);
     clearResetScreen(screen);
     screen->print("Mantle &");
+    screen->setCursor(0, 1);
+    screen->print("OverJerry &");
     screen->setCursor(0, 1);
     screen->print("iRed_K");
     delay(1000);
@@ -253,6 +262,16 @@ void initDht() {
     CONST_CAST(logger, Logger *)->Info("Done.");
 }
 
+void initPin() {
+    for (int target_id = 22; target_id <= 35; target_id++) {
+        CONST_CAST(logger, Logger *)->Debug(String("OFF action on ") + String(target_id));
+        digitalWrite(target_id, LOW);
+    }
+    for (int i = 0; i<=11 ;i++) {
+        pinState[i] = false;
+    }
+}
+
 void setup() {
     //Startup scripts
     initSerial();
@@ -260,6 +279,7 @@ void setup() {
     printLicenseInfo();
     initEthernet();
     initDht();
+    initPin();
 
     CONST_CAST(logger, Logger *)->Info("Init done.");
 }
@@ -273,18 +293,101 @@ void loop() {
     CONST_CAST(logger, Logger *)->Info("Uploading sensor data");
     if (webUploader->connect(webServerIp, webServerPort)) {
         CONST_CAST(logger, Logger *)->Debug("Connection established.");
+        cJSON * root =  cJSON_CreateObject();
 
-        webUploader->print(String("GET /api/v1.1/upload.php?air_temp=") + String(currentAirTemp) \
-                + String("&air_hum=") + String(currentAirHum) \
-                + String("&air_light=") + String(currentLightValue) \
-                + String("&ground_hum=") + String(currentGroundHum) \
+        cJSON_AddItemToObject(root, "version", cJSON_CreateString("1.2.0"));
+
+        cJSON * timestamp = cJSON_AddObjectToObject(root, "timestamp");
+        cJSON_AddItemToObject(timestamp, "hour", cJSON_CreateNumber(p->tm_hour));
+        cJSON_AddItemToObject(timestamp, "minute", cJSON_CreateNumber(p->tm_min));
+        cJSON_AddItemToObject(timestamp, "second", cJSON_CreateNumber(p->tm_sec));
+
+        cJSON * data = cJSON_AddArrayToObject(root, "data");
+        cJSON * data_air_temp = cJSON_CreateObject();
+        cJSON * data_air_hum = cJSON_CreateObject();
+        cJSON * data_light_value = cJSON_CreateObject();
+        cJSON * data_ground_hum = cJSON_CreateObject();
+        cJSON_AddItemToObject(data_air_temp, "data_type", cJSON_CreateNumber(0));
+        cJSON_AddItemToObject(data_air_temp, "data_content", cJSON_CreateNumber(currentAirTemp));
+        cJSON_AddItemToObject(data_air_hum, "data_type", cJSON_CreateNumber(1));
+        cJSON_AddItemToObject(data_air_hum, "data_content", cJSON_CreateNumber(currentAirHum));
+        cJSON_AddItemToObject(data_light_value, "data_type", cJSON_CreateNumber(2));
+        cJSON_AddItemToObject(data_light_value, "data_content", cJSON_CreateNumber(currentLightValue));
+        cJSON_AddItemToObject(data_ground_hum, "data_type", cJSON_CreateNumber(3));
+        cJSON_AddItemToObject(data_ground_hum, "data_content", cJSON_CreateNumber(currentGroundHum));
+        cJSON_AddItemToArray(data, data_air_temp);
+        cJSON_AddItemToArray(data, data_air_hum);
+        cJSON_AddItemToArray(data, data_light_value);
+        cJSON_AddItemToArray(data, data_ground_hum);
+
+        cJSON * state = cJSON_AddArrayToObject(root, "state");
+        cJSON * state_water = cJSON_CreateObject();         
+        cJSON * state_fan_one = cJSON_CreateObject();
+        cJSON * state_fan_two = cJSON_CreateObject();
+        cJSON * state_air = cJSON_CreateObject();
+        cJSON * state_windows_side_open = cJSON_CreateObject();
+        cJSON * state_windows_side_close = cJSON_CreateObject();
+        cJSON * state_windows_one_open = cJSON_CreateObject();
+        cJSON * state_windows_one_close = cJSON_CreateObject();
+        cJSON * state_windows_two_open = cJSON_CreateObject();
+        cJSON * state_windows_two_close = cJSON_CreateObject();
+        cJSON * state_sheet_outer_open = cJSON_CreateObject();
+        cJSON * state_sheet_outer_close = cJSON_CreateObject();
+        cJSON * state_sheet_inner_open = cJSON_CreateObject();
+        cJSON * state_sheet_inner_close = cJSON_CreateObject();
+        cJSON_AddItemToObject(state_water, "pin_id", cJSON_CreateNumber(22));
+        cJSON_AddItemToObject(state_water, "state", cJSON_CreateNumber(pinState[0]));
+        cJSON_AddItemToObject(state_fan_one, "pin_id", cJSON_CreateNumber(23));
+        cJSON_AddItemToObject(state_fan_one, "state", cJSON_CreateNumber(pinState[1]));
+        cJSON_AddItemToObject(state_fan_two, "pin_id", cJSON_CreateNumber(24));
+        cJSON_AddItemToObject(state_fan_two, "state", cJSON_CreateNumber(pinState[2]));
+        cJSON_AddItemToObject(state_air, "pin_id", cJSON_CreateNumber(25));
+        cJSON_AddItemToObject(state_air, "state", cJSON_CreateNumber(pinState[3]));
+        cJSON_AddItemToObject(state_windows_side_open, "pin_id", cJSON_CreateNumber(26));
+        cJSON_AddItemToObject(state_windows_side_open, "state", cJSON_CreateNumber(pinState[4]));
+        cJSON_AddItemToObject(state_windows_side_close, "pin_id", cJSON_CreateNumber(27));
+        cJSON_AddItemToObject(state_windows_side_close, "state", cJSON_CreateNumber(pinState[5]));
+        cJSON_AddItemToObject(state_windows_one_open, "pin_id", cJSON_CreateNumber(28));
+        cJSON_AddItemToObject(state_windows_one_open, "state", cJSON_CreateNumber(pinState[6]));
+        cJSON_AddItemToObject(state_windows_one_close, "pin_id", cJSON_CreateNumber(29));
+        cJSON_AddItemToObject(state_windows_one_close, "state", cJSON_CreateNumber(pinState[7]));
+        cJSON_AddItemToObject(state_windows_two_open, "pin_id", cJSON_CreateNumber(30));
+        cJSON_AddItemToObject(state_windows_two_open, "state", cJSON_CreateNumber(pinState[8]));
+        cJSON_AddItemToObject(state_windows_two_close, "pin_id", cJSON_CreateNumber(31));
+        cJSON_AddItemToObject(state_windows_two_close, "state", cJSON_CreateNumber(pinState[9]));
+        cJSON_AddItemToObject(state_sheet_outer_open, "pin_id", cJSON_CreateNumber(32));
+        cJSON_AddItemToObject(state_sheet_outer_open, "state", cJSON_CreateNumber(pinState[10]));
+        cJSON_AddItemToObject(state_sheet_outer_close, "pin_id", cJSON_CreateNumber(33));
+        cJSON_AddItemToObject(state_sheet_outer_close, "state", cJSON_CreateNumber(pinState[11]));
+        cJSON_AddItemToObject(state_sheet_inner_open, "pin_id", cJSON_CreateNumber(34));
+        cJSON_AddItemToObject(state_sheet_inner_open, "state", cJSON_CreateNumber(pinState[12]));
+        cJSON_AddItemToObject(state_sheet_inner_close, "pin_id", cJSON_CreateNumber(35));
+        cJSON_AddItemToObject(state_sheet_inner_close, "state", cJSON_CreateNumber(pinState[13]));
+        cJSON_AddItemToArray(state, state_water);        
+        cJSON_AddItemToArray(state, state_fan_one);
+        cJSON_AddItemToArray(state, state_fan_two);
+        cJSON_AddItemToArray(state, state_air);
+        cJSON_AddItemToArray(state, state_windows_side_open);
+        cJSON_AddItemToArray(state, state_windows_side_close);
+        cJSON_AddItemToArray(state, state_windows_one_open);
+        cJSON_AddItemToArray(state, state_windows_one_close);
+        cJSON_AddItemToArray(state, state_windows_two_open);
+        cJSON_AddItemToArray(state, state_windows_two_close);
+        cJSON_AddItemToArray(state, state_sheet_outer_open);
+        cJSON_AddItemToArray(state, state_sheet_outer_close);
+        cJSON_AddItemToArray(state, state_sheet_inner_open);
+        cJSON_AddItemToArray(state, state_sheet_inner_close);
+
+        webUploader->print(String("POST /api/v1.2/upload.php")
                 + String(" HTTP/1.1\r\n" \
                 "Accept: application/json\r\n" \
                 "Host: ") + String(webServerAddress) + String(":") + String(webServerPort) + String("\r\n") + String( \
                 "User-Agent: arduino/mega2560\r\n" \
                 "Connection: close\r\n" \
                 "\r\n" \
-                ""));
+                ) +
+                String(cJSON_Print(root)) \
+                );
         {
             String respond = String();
             do
